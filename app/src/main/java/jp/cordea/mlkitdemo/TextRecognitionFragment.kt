c0bdee3954631eb3b0cbd9d970c9ed915fa0b93b
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-18 17:59:31 +0900

    run text recognition

--- app/src/main/java/jp/cordea/mlkitdemo/TextRecognitionFragment.kt
+++ app/src/main/java/jp/cordea/mlkitdemo/TextRecognitionFragment.kt
@@ -3,11 +3,15 @@ package jp.cordea.mlkitdemo
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import kotlinx.android.synthetic.main.fragment_text_recognition.*
 
 class TextRecognitionFragment : Fragment() {
@@ -39,6 +43,26 @@ class TextRecognitionFragment : Fragment() {
             return
         }
         if (requestCode == REQUEST_CODE) {
+            handleImage(uri)
         }
+    }
+
+    private fun handleImage(uri: Uri) {
+        progressBar.isVisible = true
+        contentGroup.isVisible = false
+        textView.text = ""
+        val image = FirebaseVisionImage.fromFilePath(requireContext(), uri)
+        val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
+        detector.processImage(image)
+            .addOnCompleteListener {
+                progressBar.isVisible = false
+                contentGroup.isVisible = true
+            }
+            .addOnSuccessListener {
+                textView.text = it.text
+            }
+            .addOnFailureListener {
+                it.printStackTrace()
+            }
     }
 }
