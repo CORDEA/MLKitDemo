387caeb697109ecff648304b852f820372865467
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-18 17:02:47 +0900

    navigate to TextRecognitionFragment

--- app/src/main/java/jp/cordea/mlkitdemo/MainFragment.kt
+++ app/src/main/java/jp/cordea/mlkitdemo/MainFragment.kt
@@ -6,14 +6,21 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
 
 class MainFragment : Fragment() {
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+    ): View = inflater.inflate(R.layout.fragment_main, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        textRecognitionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_textRecognitionFragment)
+        }
     }
 }
