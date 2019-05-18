77884b74740eb99cb97d09c31f888de20243b914
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-18 17:30:03 +0900

    choose picture

--- app/src/main/java/jp/cordea/mlkitdemo/TextRecognitionFragment.kt
+++ app/src/main/java/jp/cordea/mlkitdemo/TextRecognitionFragment.kt
@@ -1,19 +1,44 @@
 package jp.cordea.mlkitdemo
 
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_text_recognition.*
 
 class TextRecognitionFragment : Fragment() {
+    companion object {
+        private const val REQUEST_CODE = 1
+    }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_text_recognition, container, false)
+    ): View = inflater.inflate(R.layout.fragment_text_recognition, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        chooseButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
+                addCategory(Intent.CATEGORY_OPENABLE)
+                type = "image/*"
+            }
+            startActivityForResult(Intent.createChooser(intent, "Choose picture"), REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val uri = data?.data ?: return
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        if (requestCode == REQUEST_CODE) {
+        }
     }
 }
