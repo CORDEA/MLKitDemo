248bcb13c2c8271206aaff56d705b743a6bf8c50
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-19 12:18:57 +0900

    navigate to LandmarkRecognitionFragment

--- app/src/main/java/jp/cordea/mlkitdemo/MainFragment.kt
+++ app/src/main/java/jp/cordea/mlkitdemo/MainFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_main.*
 
@@ -35,5 +36,8 @@ class MainFragment : Fragment() {
             findNavController()
                 .navigate(R.id.action_mainFragment_to_objectDetectionAndTrackingFragment)
         }
+        landmarkRecognitionButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.landmarkRecognitionFragment)
+        )
     }
 }
