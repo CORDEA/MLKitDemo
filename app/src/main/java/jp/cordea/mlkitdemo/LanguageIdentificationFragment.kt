acf5fe8bbe95ced5d999a4f771a7e91df340226b
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-19 15:41:30 +0900

    add new fragment

---
+++ app/src/main/java/jp/cordea/mlkitdemo/LanguageIdentificationFragment.kt
@@ -0,0 +1,16 @@
+package jp.cordea.mlkitdemo
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class LanguageIdentificationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_language_identification, container, false)
+}
