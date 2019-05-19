867c787415e790ae579907c1e49f74f96b12fd7b
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-19 15:54:18 +0900

    identify language

--- app/src/main/java/jp/cordea/mlkitdemo/LanguageIdentificationFragment.kt
+++ app/src/main/java/jp/cordea/mlkitdemo/LanguageIdentificationFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage
+import kotlinx.android.synthetic.main.fragment_language_identification.*
 
 class LanguageIdentificationFragment : Fragment() {
 
@@ -13,4 +15,20 @@ class LanguageIdentificationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_language_identification, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        button.setOnClickListener {
+            val text = editText.text.toString()
+            identifyLanguage(text)
+        }
+    }
+
+    private fun identifyLanguage(text: String) {
+        FirebaseNaturalLanguage.getInstance()
+            .languageIdentification
+            .identifyLanguage(text)
+            .addOnSuccessListener { textView.text = it }
+            .addOnFailureListener { it.printStackTrace() }
+    }
 }
