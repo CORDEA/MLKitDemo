ef9e83d2621621344f346b1fb00792feae4a586b
Yoshihiro Tanaka <contact@cordea.jp>
2019-05-19 10:47:52 +0900

    label objects

--- app/src/main/java/jp/cordea/mlkitdemo/ImageLabelingFragment.kt
+++ app/src/main/java/jp/cordea/mlkitdemo/ImageLabelingFragment.kt
@@ -2,12 +2,15 @@ package jp.cordea.mlkitdemo
 
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import kotlinx.android.synthetic.main.fragment_image_labeling.*
 
 class ImageLabelingFragment : Fragment() {
@@ -22,6 +25,9 @@ class ImageLabelingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         uiBinder.onResult.observe(this, Observer {
+            when (it) {
+                is ImageChoosableUiBinder.Result.ReceivedLocalImage -> handleImage(it.uri)
+            }
         })
         uiBinder.bind(chooseButton)
     }
@@ -29,5 +35,15 @@ class ImageLabelingFragment : Fragment() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         uiBinder.handleActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun handleImage(uri: Uri) {
+        val image = FirebaseVisionImage.fromFilePath(requireContext(), uri)
+        val labeler = FirebaseVision.getInstance().onDeviceImageLabeler
+
+        labeler.processImage(image)
+            .addOnSuccessListener { result ->
+            }
+            .addOnFailureListener { it.printStackTrace() }
     }
 }
